b96486920f825481f1709de88609868a49057232
HaikalAditya <haikaladitya6@gmail.com>
2024-03-31 08:51:42 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println("== Data Mahasiswa ======================");
+  System.out.println("NPM  : 2210010211");
+  System.out.println("Nama : Haikal Aditya Rahmatullah");
+  System.out.println("========================================");
+ }
+}
